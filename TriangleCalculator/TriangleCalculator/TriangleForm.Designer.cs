ea641e61dc7ac8d01e480938eccd65e5a7c0c289
Andrew <10296386@uvlink.uvu.edu>
2016-10-26 13:54:43 -0600

    Initial commit

---
+++ TriangleCalculator/TriangleCalculator/TriangleForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace TriangleCalculator
+{
+    partial class TriangleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSideA = new System.Windows.Forms.Label();
+            this.lblSideB = new System.Windows.Forms.Label();
+            this.lblSideC = new System.Windows.Forms.Label();
+            this.nudAInput = new System.Windows.Forms.NumericUpDown();
+            this.nudBInput = new System.Windows.Forms.NumericUpDown();
+            this.nudCInput = new System.Windows.Forms.NumericUpDown();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSideA
+            // 
+            this.lblSideA.AutoSize = true;
+            this.lblSideA.Location = new System.Drawing.Point(53, 60);
+            this.lblSideA.Name = "lblSideA";
+            this.lblSideA.Size = new System.Drawing.Size(69, 13);
+            this.lblSideA.TabIndex = 0;
+            this.lblSideA.Text = "Side a length";
+            // 
+            // lblSideB
+            // 
+            this.lblSideB.AutoSize = true;
+            this.lblSideB.Location = new System.Drawing.Point(53, 107);
+            this.lblSideB.Name = "lblSideB";
+            this.lblSideB.Size = new System.Drawing.Size(69, 13);
+            this.lblSideB.TabIndex = 1;
+            this.lblSideB.Text = "Side b length";
+            // 
+            // lblSideC
+            // 
+            this.lblSideC.AutoSize = true;
+            this.lblSideC.Location = new System.Drawing.Point(53, 160);
+            this.lblSideC.Name = "lblSideC";
+            this.lblSideC.Size = new System.Drawing.Size(69, 13);
+            this.lblSideC.TabIndex = 2;
+            this.lblSideC.Text = "Side c length";
+            // 
+            // nudAInput
+            // 
+            this.nudAInput.Location = new System.Drawing.Point(155, 53);
+            this.nudAInput.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudAInput.Name = "nudAInput";
+            this.nudAInput.Size = new System.Drawing.Size(120, 20);
+            this.nudAInput.TabIndex = 3;
+            this.nudAInput.ValueChanged += new System.EventHandler(this.nudAInput_ValueChanged);
+            // 
+            // nudBInput
+            // 
+            this.nudBInput.Location = new System.Drawing.Point(155, 100);
+            this.nudBInput.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudBInput.Name = "nudBInput";
+            this.nudBInput.Size = new System.Drawing.Size(120, 20);
+            this.nudBInput.TabIndex = 4;
+            this.nudBInput.ValueChanged += new System.EventHandler(this.nudBInput_ValueChanged);
+            // 
+            // nudCInput
+            // 
+            this.nudCInput.Location = new System.Drawing.Point(155, 153);
+            this.nudCInput.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudCInput.Name = "nudCInput";
+            this.nudCInput.Size = new System.Drawing.Size(120, 20);
+            this.nudCInput.TabIndex = 5;
+            this.nudCInput.ValueChanged += new System.EventHandler(this.nudCInput_ValueChanged);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 223);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(408, 54);
+            this.rtbOutput.TabIndex = 6;
+            this.rtbOutput.Text = "";
+            // 
+            // TriangleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 329);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.nudCInput);
+            this.Controls.Add(this.nudBInput);
+            this.Controls.Add(this.nudAInput);
+            this.Controls.Add(this.lblSideC);
+            this.Controls.Add(this.lblSideB);
+            this.Controls.Add(this.lblSideA);
+            this.Name = "TriangleForm";
+            this.Text = "Triangle Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.nudAInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudBInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSideA;
+        private System.Windows.Forms.Label lblSideB;
+        private System.Windows.Forms.Label lblSideC;
+        private System.Windows.Forms.NumericUpDown nudAInput;
+        private System.Windows.Forms.NumericUpDown nudBInput;
+        private System.Windows.Forms.NumericUpDown nudCInput;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+    }
+}
+
---
+++ TriangleCalculator/TriangleCalculator/TriangleForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriangleCalculator
+{
+    public partial class TriangleForm : Form
+    {
+
+        private decimal _aValue;
+        private decimal _bValue;
+        private decimal _cValue;
+
+        private string _sideTriangleType;
+        private string _angleTriangleType;
+
+        private RichTextBox _rtbOutput;
+
+        public TriangleForm()
+        {
+            InitializeComponent();
+
+            _rtbOutput = rtbOutput;
+        }
+
+        private void nudAInput_ValueChanged(object sender, EventArgs e)
+        {
+            _aValue = nudAInput.Value;
+            CalculateTriangle();
+        }
+
+        private void nudBInput_ValueChanged(object sender, EventArgs e)
+        {
+            _bValue = nudBInput.Value;
+            CalculateTriangle();
+        }
+
+        private void nudCInput_ValueChanged(object sender, EventArgs e)
+        {
+            _cValue = nudCInput.Value;
+            CalculateTriangle();
+        }
+
+        private void CalculateTriangle()
+        {
+            if (ThreeInputsGreaterThanZero())
+            {
+                if (CheckForValidTriangle())
+                { 
+                    CalculateTriangleSideType();
+                    CalculateTriangleAngleType();
+                    WriteToOutput("These side lengths produce a valid " + _sideTriangleType.ToString() + " " + _angleTriangleType.ToString() + " triangle");
+                }
+                else
+                {
+                    WriteToOutput("These side lengths don't produce a valid triangle");
+                }
+            }
+            else
+            {
+                WriteToOutput("All values must be greater than zero");
+            }
+        }
+
+        private bool ThreeInputsGreaterThanZero()
+        {
+            if (_aValue > 0 && _bValue > 0 && _cValue > 0)
+            {
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+        }
+
+        private bool CheckForValidTriangle()
+        {
+            decimal[] tempArray = new decimal[] { _aValue, _bValue, _cValue };
+            decimal smallestNumber = tempArray.Min(x => x);
+            decimal secondSmalletNumber = tempArray.OrderBy(x => x).Skip(1).First();
+            decimal largestNumber = tempArray.Max(x => x);
+            decimal sumOfSmallest = smallestNumber + secondSmalletNumber;
+
+            if (sumOfSmallest > largestNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void CalculateTriangleSideType()
+        {
+            decimal[] tempArray = new decimal[] { _aValue, _bValue, _cValue };
+
+            // check for Scalene
+            if (tempArray.GroupBy(x => x).All(y => y.Count() == 1))
+            {
+                _sideTriangleType = "Scalene";
+            }
+            // check for Equilateral
+            else if (tempArray.GroupBy(x => x).Any(y => y.Count() > 2))
+            {
+                _sideTriangleType = "Equilateral";
+            }
+            // is Isosceles 
+            else
+            {
+                _sideTriangleType = "Isosceles";
+            }
+        }
+
+        private void CalculateTriangleAngleType()
+        {
+            double aValue = Convert.ToDouble(_aValue);
+            double bValue = Convert.ToDouble(_bValue);
+            double cValue = Convert.ToDouble(_cValue);
+
+            // cosine rule a² = b² + c² - 2bccosA
+            // A = cos^-1((b² + c² - a²)/(2bc)
+            double angleA = Math.Acos((Math.Pow(bValue, 2) + Math.Pow(cValue, 2) - Math.Pow(aValue, 2)) / (2 * bValue * cValue)) * (180/Math.PI);
+            double angleB = Math.Acos((Math.Pow(aValue, 2) + Math.Pow(cValue, 2) - Math.Pow(bValue, 2)) / (2 * aValue * cValue)) * (180 / Math.PI);
+            double angleC = Math.Acos((Math.Pow(aValue, 2) + Math.Pow(bValue, 2) - Math.Pow(cValue, 2)) / (2 * aValue * bValue)) * (180 / Math.PI);
+
+            double[] tempArray = new double[] { angleA, angleB, angleC };
+            // check for Equiangular 
+            if (tempArray.GroupBy(x => x).Any(y => y.Count() > 2))
+            {
+                _angleTriangleType = "Equiangular";
+            }
+            // check for Right 
+            else if (tempArray.Any(x => x == 90))
+            {
+                _angleTriangleType = "Right";
+            }
+            // check for Obtuse 
+            else if (tempArray.Any(x => x > 90))
+            {
+                _angleTriangleType = "Obtuse";
+            }
+            // is Acute  
+            else
+            {
+                _angleTriangleType = "Acute";
+            }
+        }
+
+        private void WriteToOutput(string outputString)
+        {
+            _rtbOutput.Clear();
+            _rtbOutput.AppendText(outputString);
+        }
+    }
+}
